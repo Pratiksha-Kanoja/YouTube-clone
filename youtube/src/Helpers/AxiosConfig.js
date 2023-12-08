f34e2e527250d6982211d888867a4a3e0111f7db
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-12-08 10:41:07 +0530

    Update AxiosConfig.js

--- youtube/src/Helpers/AxiosConfig.js
+++ youtube/src/Helpers/AxiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-var BackendUrl = "http://localhost:8000/api/v1"
+//var BackendUrl = "http://localhost:8000/api/v1"
 
-if(process.env.MODE === "PRODUCTION"){
-    BackendUrl = "https://youtube-clone-br6k.onrender.com/api/v1"
-}
+// if(process.env.MODE === "PRODUCTION"){
+   var BackendUrl = "https://youtube-clone-br6k.onrender.com/api/v1"
+//
     const api = axios.create({baseURL: BackendUrl})
 
 export default api;
